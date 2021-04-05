8930b6c1d4cd6230b9c341a58df60f0f3da8fc77
Jimmy Vrana <jimmydv6@yahoo.com>
2021-04-04 19:30:20 -0500

    implemented material-ui & fixed a few bugs.

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Garage from './pages/Garage';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core';
+
 
 const Home = ({handleLogout}) => {
-
-    return(
-        <section className="home">
-            <nav>
-                <Router>
-                    <Navbar>
-                        <Switch>
-                            <Route path='/' exact component={Home} />
-                            <Route path='/garage' component={Garage} />
-                        </Switch>
-                    </Navbar>
-                </Router>
-                <h2>Welcome to Trakked</h2>
-                <button onClick={handleLogout}>Logout</button>
-            </nav>
-        </section>
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton edge="start" className="menuButton" color="inherit" aria-label="menu">
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" className="barTitle">
+                    Welcome to Trakked
+                </Typography>
+                <Button onClick={handleLogout} color="inherit">Logout</Button>
+            </Toolbar>
+        </AppBar>
     );
 };
+// const Home = ({handleLogout}) => {
+
+//     return(
+//         <section className="home">
+//             <nav>
+//                 <Router>
+//                     <Navbar>
+//                         <Switch>
+//                             <Route path='/' exact component={Home} ><Home></Route>
+//                             <Route path='/garage' component={Garage} />
+//                         </Switch>
+//                     </Navbar>
+//                 </Router>
+//                 <h2>Welcome to Trakked</h2>
+//                 <button onClick={handleLogout}>Logout</button>
+//             </nav>
+//         </section>
+//     );
+// };
 
 export default Home;
--- src/Login.js
+++ src/Login.js
@@ -1,4 +1,7 @@
+import { Button, Input, InputLabel, Paper, TextField } from '@material-ui/core';
 import React from 'react';
+import logoImg from './images/trklogo-transparent.png';
+
 
 const Login = (props) => {
 
@@ -20,55 +23,61 @@ const Login = (props) => {
 
 
  
-        
-        <section className="login">
-            <header> 
-                <img src={require("./images/trklogo.png")}/>    
-            </header>
-            <section id="main">
-            <div className="main-text">
-                Trakked. <br></br> where we reinvent <br /> 
-                the meaning of automotive maintenance
-            </div>
+            <Paper className="header" square elevation={3} style={{backgroundColor: "#222430", position: "absolute", width: "100%"}}> 
+                <img src={logoImg} width="150px" height="150px"/>   
+            </Paper>
+
+            <section className="login">
+                
+                <section id="main">
+                    <div className="text-dk-bg">
+                        <p className="main-text">
+                            Trakked. <br></br> where we reinvent <br /> 
+                            the meaning of automotive maintenance.
+                        </p>
+                    </div>
+                </section>
+                <Paper className="loginContainer">
+                    <InputLabel htmlFor="email-input" className="text-dk-bg">Email Address</InputLabel>
+                    <Input 
+                    id="email-input"
+                    type="email"
+                    autoFocus 
+                    autoComplete
+                    required value={email} 
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <p className="errorMsg">{emailError}</p>
+                    <InputLabel htmlFor="pwd-input" className="text-dk-bg">Password</InputLabel>
+                    <Input id="pwd-input"
+                    autoComplete
+                    type="password"
+                    required value={password}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <p className="errorMsg">{passwordError}</p>
+                    <div className="btnContainer">
+                        {hasAccount ? (
+                            <>
+                                <Button variant="contained" onClick={handleLogin}>Sign in</Button>
+                                <p>
+                                    Don't have an account? 
+                                    <span onClick={() => setHasAccount(!hasAccount)}>Sign up</span>
+                                </p>
+                            </>
+                        ) : (
+                            <>
+                                <Button variant="contained" onClick={handleSignup}>Sign up</Button>
+                                <p>
+                                    Have an account? 
+                                    <span onClick={() => setHasAccount(!hasAccount)}>Sign in</span>
+                                </p>
+                            </>
+                        )}
+                    </div>
+                </Paper>
             </section>
-            <div className="loginContainer">
-                <label>Username</label>
-                <input 
-                type="text" 
-                autoFocus 
-                required value={email} 
-                onChange={(e) => setEmail(e.target.value)}
-                />
-                <p className="errorMsg">{emailError}</p>
-                <label>Password</label>
-                <input 
-                type="password"
-                required value={password}
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                />
-                <p className="errorMsg">{passwordError}</p>
-                <div className="btnContainer">
-                    {hasAccount ? (
-                        <>
-                            <button onClick={handleLogin}>Sign in</button>
-                            <p>
-                                Don't have an account? 
-                                <span onClick={() => setHasAccount(!hasAccount)}>Sign up</span>
-                            </p>
-                        </>
-                    ) : (
-                        <>
-                            <button onClick={handleSignup}>Sign up</button>
-                            <p>
-                                Have an account? 
-                                <span onClick={() => setHasAccount(!hasAccount)}>Sign in</span>
-                            </p>
-                        </>
-                    )}
-                </div>
-            </div>
-        </section>
         </>
     );
 };
