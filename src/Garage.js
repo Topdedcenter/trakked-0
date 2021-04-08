b3d526d66ad1e4a0c705474d0e8451b16bca2322
vijaysingh0315 <79027590+vijaysingh0315@users.noreply.github.com>
2021-04-08 14:15:43 -0500

    Add files via upload

---
+++ src/Garage.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import fire from './fire';
+
+const CarForm = () => {
+    const initialFieldValues = {
+        year: '',
+        make: '',
+        model: '',
+    }
+}
+
+export default Garage;
---
+++ src/Maintenance.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Maintenance() {
+    return (
+        <div className='maintenance'>
+            <h1>Maintenance</h1>
+        </div>
+    );
+}
+
+export default Maintenance;
